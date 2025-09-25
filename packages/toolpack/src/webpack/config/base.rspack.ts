f26d0ec7cec77e2d4197ced36cddf2b8f5d846cb
loren-z <loren.z@binance.com>
2025-09-25 14:23:16 +0800

    feat: only use analyze in client side for optimize

--- packages/toolpack/src/webpack/config/base.rspack.ts
+++ packages/toolpack/src/webpack/config/base.rspack.ts
@@ -168,8 +168,11 @@ export function baseRspackChain({
      */
     // config.optimization.minimizer('terser').use(TerserPlugin, [ ... ]);
     // config.optimization.minimizer('cssMinimizer').use(CssMinimizerPlugin, [ ... ]);
-
-    if (analyze && name === 'shuvi/client') {
+    const shouldAnalyze = analyze && (
+      name === 'shuvi/client' || 
+      (name === 'shuvi/server' && process.env.ANALYZE_SERVER)
+    );
+    if (shouldAnalyze) {
       const targetName = getSimplifiedTargetName(name);
       config
         .plugin('private/bundle-analyzer-plugin')
